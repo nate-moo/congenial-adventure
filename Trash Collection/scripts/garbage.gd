7e12357ddcee88a8e9158773eb828d29e2bd3744
Sophie Anderson <mythicdagger713@gmail.com>
2024-02-10 13:08:29 -0700

    edit sprite size and mechanics and player speed, initial hud

--- Trash Collection/scripts/garbage.gd
+++ Trash Collection/scripts/garbage.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 signal screen_exited
+signal collected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var trash_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(trash_types[randi() % trash_types.size()])
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
@@ -12,3 +14,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass # is code needed here?
+
+func _on_body_entered(body):
+	hide()
+	collected.emit()
+	#$CollisionShape2D.set_deferred("disabled", true)
