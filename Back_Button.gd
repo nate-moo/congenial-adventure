5fd19944bbfff7a7b7227c01241d7cfd066a376e
naed3r <40650681+nate-moo@users.noreply.github.com>
2024-02-09 21:27:30 -0700

    Connecting scenes together

--- Back_Button.gd
+++ Back_Button.gd
@@ -1,11 +1,10 @@
-extends Node
+extends Button
 
-var minigames_scene = preload("res://minigames_scene.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,5 +12,5 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://minigames_scene.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 	pass # Replace with function body.
