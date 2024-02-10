af5b2e38107837c356b7ca6a597c7691c7842af3
Sophie Anderson <mythicdagger713@gmail.com>
2024-02-10 00:04:25 -0700

    fixed the majority of bugs, mostly due to incorrect function names because I have fat fingers

--- scripts/trash_collection/garbage.gd
+++ scripts/trash_collection/garbage.gd
@@ -3,7 +3,7 @@ signal screen_exited
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var trash_types = $AnimatedSprite2D.sprite_frames.get_animation_frames()
+	var trash_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(trash_types[randi() % trash_types.size()])
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
