07d307b2079b1e1a78e720f80cd4cb7710370a52
Morgan <39073323+Morgan-Steele@users.noreply.github.com>
2024-02-11 00:26:24 -0700

    Fire Sounds

--- Tree Mini/Tree_Sprites.gd
+++ Tree Mini/Tree_Sprites.gd
@@ -4,6 +4,7 @@ var width
 var height
 var rand_num
 var color
+var score
 #var current_tree = $TreeBurning
 
 func passData(width_pass, height_pass):
@@ -23,11 +24,18 @@ func _ready():
 func _on_tree_burning_animation_looped():
 	if ($TreeBurning.animation == "Green Burn" or $TreeBurning.animation == "Orange Burn"):
 		$TreeBurning.queue_free()
+		score += 1
 
 func _on_area_2d_2_area_entered(area):
 	if self.get_parent() != area.get_parent():
 		if (color == "green"):
 			$TreeBurning.play("Green Burn")
+			$"Fire Sounds".play()
 		if (color == "orange"):
 			$TreeBurning.play("Orange Burn")
+			$"Fire Sounds".play()
+
+func _on_fire_sounds_finished():
+	#if ($TreeBurning.animation == "Green Burn" or $TreeBurning.animation == "Orange Burn"):
+	$"Fire Sounds".play()
 
