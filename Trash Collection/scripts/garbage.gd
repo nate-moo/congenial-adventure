b9fe548482f57d03228eff03484573521d9351f6
Sophie Anderson <mythicdagger713@gmail.com>
2024-02-10 20:32:17 -0700

    removed duplicate garbage.gd file

--- Trash Collection/scripts/garbage.gd
+++ Trash Collection/scripts/garbage.gd
@@ -1,21 +1,25 @@
 extends RigidBody2D
 signal screen_exited
-signal collected
+#signal collected # should be handled by player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var trash_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(trash_types[randi() % trash_types.size()])
-
+	#var velocity = Vector2.ZERO # trash movement vector?
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	queue_free() # if implementing litterer?
 
-func _on_body_entered(body):
+func _on_Garbage_body_entered(body): #might be an area function
 	hide()
-	collected.emit()
+#	collected.emit() # disabled since player (theoretically) handles collisions
 	$CollisionShape2D.set_deferred("disabled", true)
+
+#func _on_collected():
+	#pass # Replace with function body.	
 	
 #keeping just in case needed
 func _process(delta):
 	pass
+
+
