58adbc0d526063635d2a0050556bf1a74c4e07ac
Morgan <39073323+Morgan-Steele@users.noreply.github.com>
2024-02-10 21:51:51 -0700

    Idk

--- Tree Mini/new_player.gd
+++ Tree Mini/new_player.gd
@@ -1,22 +1,21 @@
 extends Area2D
 signal hit
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export var speed = 400
+var screen_size
 var angular_speed = PI
 var screen_size_original
 var character_size
 @export var shape_scale_factor_x = 1.6
 @export var shape_scale_factor_y = 3.4
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size_original = get_viewport_rect().size
 	screen_size = screen_size_original
-	
+
 	# X scaling
 	character_size = $CollisionShape2D.shape.get_rect().size.x * shape_scale_factor_x
 	screen_size.x -= character_size
-	
+
 	# Y scaling
 	character_size = $CollisionShape2D.shape.get_rect().size.y * shape_scale_factor_y
 	screen_size.y -= character_size
@@ -35,11 +34,25 @@ func _process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("tree_player_back"):
 		velocity.y += 1
+	
 	if (Input.is_action_pressed("BlowTorch") && velocity.x == -1):
 		$"CollisionShape2D/Fire blowtorch".play("Walk left")
-	if (Input.is_action_pressed("BlowTorch") && velocity.x == 1):
+		$CollisionShape2D/BlowtorchPlayer/Area2D2/CollisionShape2D3.disabled = false
+		$CollisionShape2D/BlowtorchPlayer/Area2D/CollisionShape2D2.disabled = true
+	elif (Input.is_action_pressed("BlowTorch") && velocity.x == 1):
 		$"CollisionShape2D/Fire blowtorch".play("Walk right")
-
+		$CollisionShape2D/BlowtorchPlayer/Area2D2/CollisionShape2D3.disabled = true
+		$CollisionShape2D/BlowtorchPlayer/Area2D/CollisionShape2D2.disabled = false
+	elif (Input.is_action_pressed("BlowTorch") && velocity.y == -1):
+		$"CollisionShape2D/Fire blowtorch".play("Walk left")
+		$CollisionShape2D/BlowtorchPlayer/Area2D2/CollisionShape2D3.disabled = false
+		$CollisionShape2D/BlowtorchPlayer/Area2D/CollisionShape2D2.disabled = true
+	elif (Input.is_action_pressed("BlowTorch") && velocity.y == 1):
+		$"CollisionShape2D/Fire blowtorch".play("Walk right")
+	elif (Input.is_action_pressed("BlowTorch")):
+		$CollisionShape2D/BlowtorchPlayer/Area2D2/CollisionShape2D3.disabled = true
+		$CollisionShape2D/BlowtorchPlayer/Area2D/CollisionShape2D2.disabled = false
+		$"CollisionShape2D/Fire blowtorch".play("Walk right")
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -55,7 +68,7 @@ func _on_body_entered(body):
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
-	
+
 func start(pos):
 	position = pos
 	show()
