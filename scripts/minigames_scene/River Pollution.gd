95b7b30be530a9e349b76b81a023bb91b328a491
naed3r <40650681+nate-moo@users.noreply.github.com>
2024-02-10 23:25:53 -0700

    Added river pollution game x2

--- scripts/minigames_scene/River Pollution.gd
+++ scripts/minigames_scene/River Pollution.gd
@@ -12,5 +12,5 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://river_pollution_scene/RiverPollution.tscn")
+	get_tree().change_scene_to_file("res://river_pollution_scene/RiverView.tscn")
 	pass # Replace with function body.
